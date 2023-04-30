6d5370033c4406a9684c03cc5944c4a5cf6d022e
Caio Ramalho <103006310+RamalhoCaio@users.noreply.github.com>
2023-04-30 19:07:49 -0300

    Add files via upload

---
+++ src/components/botao.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+
+export default function abc(){
+    return(
+        <View>
+            console.log("abc")
+            <Text>Infelizmente não temos mais opçoes disponíveis</Text>
+            
+
+        </View>
+    )
+}
---
+++ src/components/teste.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native-web";
+
+ export default function Api(){
+
+    return(
+        <View>
+            <Text>Teste</Text>
+        </View>
+    );
+ }
